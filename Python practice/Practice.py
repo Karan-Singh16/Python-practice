5778ba17ad6e2209fbf12523f42b8f0a7175235f
Karanjeet singh <127685235+Karan-Singh16@users.noreply.github.com>
2024-09-21 20:05:28 +0100

    Add files via upload

---
+++ Python practice/Practice.py
@@ -0,0 +1,56 @@
+# this is for practice only and is broken down into various chunks
+
+# tutorial for beginners
+
+# This is my first Python program Lesson 1
+print("I Like Kebab!")
+print("It's really good!")
+
+
+
+# Lesson 2: Variables
+
+# Variable = A container for a value  (string, interger, float, boolean)
+# A variable behaves as if it was the value it contains
+
+# Strings
+first_name = "Karan"
+food = "pizza"
+email = "karanjeet1612@outlook.com"
+
+print(f"My name is {first_name}")
+print(f"I like {food}")
+print(f"My email is {email}")
+
+# Integers
+age = 21
+quantity = 3
+num_of_students = 30
+
+print(f"I am {age} years old")
+print(f"I am buying {quantity} items")
+print(f"My class has {num_of_students} students")
+
+# Floats
+price = 10.99
+degree = 1
+distance = 6
+
+print(f"The price is ${price}")
+print(f"My Degree is: {degree}st class")
+print(f"Today I ran {distance} miles")
+
+# Booleans
+# booleans are always capital
+is_student = False
+for_sale = True
+is_online = True
+
+if is_online:
+    print("You are online")
+else: 
+    print("You are offline")
+
+
+# Lesson 3: type casting
+
